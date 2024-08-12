774d5e59dd17c2617e2ce8d5b70a0cf98b630bff
luis-paulo-tdo <luis.paulo.tdo@gmail.com>
2024-08-12 13:08:09 -0300

    Aula #26: Criando as classes de Entidade do Modelo.

---
+++ Projeto_06_ApiCatalogo/Projeto_06_ApiCatalogo/Models/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace Projeto_06_ApiCatalogo.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImageUrl { get; set; }
+}
